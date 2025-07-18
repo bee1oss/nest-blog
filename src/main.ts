4beaf1fc4a482c5d551517f0ed605c55b03ccd36
bee1oss <begahaciyew@gmail.com>
2025-07-18 07:53:15 +0300

    schema

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 import * as dotenv from 'dotenv'
 
-dotenv.config() // .env dosyasını erkenden yükle
+dotenv.config()
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
