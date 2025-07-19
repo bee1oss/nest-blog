f56e945aee866e1fec5c1fd9e4237341b8b275fc
bee1oss <begahaciyew@gmail.com>
2025-07-19 14:04:27 +0300

    statitics is ready

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
-import { StatisticsService } from './statistics.service';
+import { Controller, Get } from '@nestjs/common'
+import { StatisticsService } from './statistics.service'
 
 @Controller('statistics')
 export class StatisticsController {
-  constructor(private readonly statisticsService: StatisticsService) {}
+	constructor(private readonly statisticsService: StatisticsService) {}
+
+	@Get('visitors')
+	async getVisitorsStats() {
+		return this.statisticsService.getVisitorsStats()
+	}
+
+	@Get('most-viewed')
+	async getMostViewed() {
+		return this.statisticsService.getMostViewedPosts()
+	}
+
+	@Get('most-liked')
+	async getMostLiked() {
+		return this.statisticsService.getMostLikedPosts()
+	}
 }
